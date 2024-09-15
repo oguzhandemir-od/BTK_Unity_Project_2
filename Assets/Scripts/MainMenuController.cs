69b80f0c1387cb64add0825540ff238459819bb3
master <yynsozturk@gmail.com>
2024-09-15 13:05:48 +0300

    scen_v1.01

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    // Play butonu için fonksiyon
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("GameScene"); // Oyun sahnenizin ismini buraya yazýn
+    }
+
+    // Credits butonu için fonksiyon
+    public void ShowCredits()
+    {
+        // Credits ekranýna geçiþ için benzer bir iþlem
+        SceneManager.LoadScene("CreditsScene"); // Yapýmcýlarýn olduðu sahne ismi
+    }
+
+    // Exit butonu için fonksiyon
+    public void ExitGame()
+    {
+        Application.Quit(); // Oyunu kapatýr
+        Debug.Log("Game is exiting"); // Editor'da çýkýþý gözlemlemek için (Oyun kapatýlýnca bu çalýþmaz)
+    }
+}
